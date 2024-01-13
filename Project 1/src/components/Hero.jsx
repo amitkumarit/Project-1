37e7c50043cf64c3a687999446bcbeba1162ccf2
amitkumarit <amiitkumar.it@gmail.com>
2024-01-14 01:43:07 +0530

    Updated

--- Project 1/src/components/Hero.jsx
+++ Project 1/src/components/Hero.jsx
@@ -3,7 +3,7 @@ const HeroSection = () => {
     return <main className="hero container">
         <div className="hero-content">
         <h1>YOUR FEET DESERVE THE BEST</h1>
-        <p>YOUR FEET DESERVE THE BEST AND WE’RE H ERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+        <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
         <div className="hero-btn">
             <button>Shop Now</button>
             <button className="secondary-btn">Category</button>
